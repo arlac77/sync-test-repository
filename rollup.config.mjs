faa34d6708d5a5545b96726cf7f7d5518e240bc1
Markus Felten <Markus.Felten@gmx.de>
2020-04-21 12:40:11 +0200

    chore(rollup): import readFileSync,resolve,json,commonjs,executable,cleanup,builtins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,13 @@
+import builtins from "builtin-modules";
+
+import cleanup from "rollup-plugin-cleanup";
+
+import executable from "rollup-plugin-executable";
+import commonjs from "@rollup/plugin-commonjs";
+
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import { readFileSync } from "fs";
 import pkg from "./package.json";
 
 export default {
